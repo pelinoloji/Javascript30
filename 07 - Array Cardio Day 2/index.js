def13b9937f1629cf2697f0e7d70c70e071234ef
pelinoloji <kayhanpelin@gmail.com>
2020-04-04 20:10:25 +0100

    Array practise using some,find,every,findIndex

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -17,12 +17,35 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const now = new Date();
+const thisYear = now.getFullYear();
+
+const isSomeNineteen = people.some(person => {
+  return thisYear - person.year >= 19;
+});
+
+console.log(isSomeNineteen);
+
 // Array.prototype.every() // is everyone 19 or older?
+
+const isEveryOneNineteen = people.every(person => {
+  return thisYear - person.year >= 19;
+});
+console.log(isEveryOneNineteen);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+const findId = comments.find(comment => comment.id === 823423);
+console.log(findId);
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+const findIdIndex = comments.findIndex(comment => comment.id === 823423);
+// console.log(findIdIndex);
+comments.splice(findIdIndex, 1);
+
+console.table(comments);
