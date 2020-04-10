917ea634bc87207d4c0f9692f56555486ab67eb0
pelinoloji <kayhanpelin@gmail.com>
2020-04-10 12:24:16 +0100

    Make key sequence detection

--- 12 - Key Sequence Detection/index.js
+++ 12 - Key Sequence Detection/index.js
@@ -1,1 +1,13 @@
-window.addEventListener("keyup", (e) => console.log(e.key));
+const pressed = [];
+const secretCode = "pelin";
+
+window.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join("").includes(secretCode)) {
+    console.log("HOORAY! 🎉");
+    cornify_add();
+  }
+  console.log(pressed);
+});
