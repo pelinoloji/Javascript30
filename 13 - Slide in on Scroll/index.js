e63a315543f8256fc1bb072d109247400afec391
pelinoloji <kayhanpelin@gmail.com>
2020-04-14 13:32:07 +0100

    Show images when scroll down

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -13,3 +13,30 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const images = document.querySelectorAll("img");
+
+function checkSlide(e) {
+  // console.count(e);
+  images.forEach((image) => {
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    // console.log(slideInAt, "slideInAt");
+
+    const imageBottom = image.offsetTop + image.height;
+    // console.log(imageBottom, "imageBottom");
+
+    const isHalfShown = slideInAt > image.offsetTop;
+    // console.log(isHalfShown, "isHalfShown");
+
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    // console.log(isNotScrolledPast, "isNotScrolledPast");
+
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
