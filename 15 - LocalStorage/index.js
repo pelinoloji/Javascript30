e93029df2c2cf3faeb44738abd16991a6653bd11
pelinoloji <kayhanpelin@gmail.com>
2020-04-15 18:48:37 +0100

    Change background image

--- 15 - LocalStorage/index.js
+++ 15 - LocalStorage/index.js
@@ -1,3 +1,17 @@
-const addItems = document.querySelector(".add-items");
+const addItems = document
+  .querySelector(".add-items")
+  .addEventListener("submit", addItem);
+
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  // console.log("ahoj");
+  console.log(this);
+
+  const item = {
+    text: "name",
+    done: false,
+  };
+}
