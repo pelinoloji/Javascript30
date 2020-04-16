8c8196424f9b2e2854991a1810d6b57db236e693
pelinoloji <kayhanpelin@gmail.com>
2020-04-16 12:11:30 +0100

    Add items on the menu list

--- 15 - LocalStorage/index.js
+++ 15 - LocalStorage/index.js
@@ -8,10 +8,33 @@ const items = [];
 function addItem(e) {
   e.preventDefault();
   // console.log("ahoj");
-  console.log(this);
+  // console.log(this);
+  const text = this.querySelector('[name="item"]').value;
 
   const item = {
-    text: "name",
+    text,
     done: false,
   };
+
+  // console.log(item);
+  items.push(item);
+  popularity(items, itemsList);
+  localStorage.setItem("items", items);
+  this.reset();
+
+  // console.log(items);
+}
+
+function popularity(dishes = [], dishesList) {
+  dishesList.innerHTML = dishes
+    .map((dish, i) => {
+      return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${
+        dish.done ? "checked" : ""
+      } />
+          <label for="item${i}">${dish.text}</label>
+        </li>`;
+    })
+    .join("");
 }
