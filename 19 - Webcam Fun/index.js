f29ffda266493eb018ef093388f39da522dcd38a
pelinoloji <kayhanpelin@gmail.com>
2020-04-24 16:08:53 +0100

    Make a webcam effects

--- 19 - Webcam Fun/index.js
+++ 19 - Webcam Fun/index.js
@@ -26,12 +26,76 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.5;
+
+    // pixels = greenScreen(pixels);
+
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
 function takePhoto() {
-  snap.duration = 2;
+  snap.currentTime = 13;
   snap.play();
+
+  const data = canvas.toDataURL("image/jpg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "beautiful");
+  link.innerHTML = `<img src="${data}" alt="image"/> `;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+  }
+  return pixels;
+}
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 550] = pixels.data[i + 0]; //red
+    pixels.data[i + 100] = pixels.data[i + 1]; //green
+    pixels.data[i - 450] = pixels.data[i + 2]; //blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
