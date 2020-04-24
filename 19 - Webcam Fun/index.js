63be698b846e3ba981a4c01142c77960d7b003a5
pelinoloji <kayhanpelin@gmail.com>
2020-04-24 10:36:53 +0100

    Add new mp3 file && Take screenshot from video

--- 19 - Webcam Fun/index.js
+++ 19 - Webcam Fun/index.js
@@ -3,3 +3,35 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(width, height);
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.duration = 2;
+  snap.play();
+}
+
+getVideo();
