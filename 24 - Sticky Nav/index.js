9169627de3a9f5c9f809cf969a4c81595de84ab7
pelinoloji <kayhanpelin@gmail.com>
2020-04-29 22:12:58 +0100

    Make a fixed navbar component

---
+++ 24 - Sticky Nav/index.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  // console.log(topOfNav, scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    // console.log(nav.offsetHeight);
+
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
